15fff53f15079af964a3ea6bb7ad8adf6002a550
eiballesterosm <eduballesteros@gmail.com>
2022-02-25 09:16:08 -0500

    FEATURE: Add DatabaseContext and entityFramework packages Add Hotel and Country classes

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
