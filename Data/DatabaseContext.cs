2712a2abaf4c38a463b205fc2657a2ff36aba6bb
eiballesterosm <eduballesteros@gmail.com>
2022-02-26 09:18:41 -0500

    FEATURE: Add SeedingData migration Add seeding data to hotels and countries

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -9,6 +9,82 @@ namespace HotelListing.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Examples records
+            modelBuilder.Entity<Country>().HasData(
+                    new Country
+                    {
+                        Id = 1,
+                        Name = "Colombia",
+                        ShortName = "CO"
+                    },
+                    new Country
+                    {
+                        Id = 2,
+                        Name = "Germany",
+                        ShortName = "GE"
+                    },
+                    new Country
+                    {
+                        Id = 3,
+                        Name = "Spain",
+                        ShortName = "SP"
+                    }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Address = "Address 1",
+                    Name = "Name 1",
+                    Rating = 4,
+                    CountryId = 1
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Address = "Address 2",
+                    Name = "Name 2",
+                    Rating = 4,
+                    CountryId = 1
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Address = "Address 3",
+                    Name = "Name 3",
+                    Rating = 4,
+                    CountryId = 1
+                },
+                new Hotel
+                {
+                    Id = 4,
+                    Address = "Address 4",
+                    Name = "Name 4",
+                    Rating = 4,
+                    CountryId = 2
+                },
+                new Hotel
+                {
+                    Id = 5,
+                    Address = "Address 5",
+                    Name = "Name 5",
+                    Rating = 4,
+                    CountryId = 2
+                },
+                new Hotel
+                {
+                    Id = 6,
+                    Address = "Address 6",
+                    Name = "Name 6",
+                    Rating = 5,
+                    CountryId = 3
+                }
+                );
+        }
+
         public DbSet<Country> Countries { get; set; }
 
         public DbSet<Hotel> Hotels { get; set; }
---
+++ Migrations/20220226141418_SeedingData.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class SeedingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 1, "Colombia", "CO" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 2, "Germany", "GE" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 3, "Spain", "SP" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Address 1", 1, "Name 1", 4.0 },
+                    { 2, "Address 2", 1, "Name 2", 4.0 },
+                    { 3, "Address 3", 1, "Name 3", 4.0 },
+                    { 4, "Address 4", 2, "Name 4", 4.0 },
+                    { 5, "Address 5", 2, "Name 5", 4.0 },
+                    { 6, "Address 6", 3, "Name 6", 5.0 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
