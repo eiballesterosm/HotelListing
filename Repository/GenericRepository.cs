8665f917bd8abf03bf7dce13b5d7547d88b4a367
eiballesterosm <eduballesteros@gmail.com>
2022-02-26 21:07:09 -0500

    FEATURE: Set up service repositories

---
+++ Repository/GenericRepository.cs
@@ -0,0 +1,96 @@
+﻿using HotelListing.Data;
+using HotelListing.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HotelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DatabaseContext databaseContext;
+        private readonly DbSet<T> db;
+        public GenericRepository(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+            this.db = this.databaseContext.Set<T>();
+        }
+
+
+        public async Task Delete(int id)
+        {
+            var entity = await this.db.FindAsync(id);
+            if (entity != null)
+            {
+                db.Remove(entity);
+            }
+            else
+            {
+                //TODO    
+            }
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            databaseContext.RemoveRange(entities);
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            IQueryable<T> query = db;
+            if (includes != null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+        }
+
+        public async Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            IQueryable<T> query = db;
+
+            if (expression != null)
+            {
+                query = query.Where(expression);
+            }
+
+            if (includes != null)
+            {
+                foreach (var includeProperty in includes)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return await query.AsNoTracking().ToListAsync();
+        }
+
+        public async Task Insert(T entity)
+        {
+            await databaseContext.AddAsync(entity);
+        }
+
+        public async Task InsertRange(IEnumerable<T> entities)
+        {
+            await databaseContext.AddRangeAsync(entities);
+        }
+
+        public void Update(T entity)
+        {
+            db.Attach(entity);
+            databaseContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using HotelListing.Data;
+using HotelListing.IRepository;
+using System;
+using System.Threading.Tasks;
+
+namespace HotelListing.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext databaseContext;
+
+        private IGenericRepository<Country> countries;
+        private IGenericRepository<Hotel> hotels;
+
+        public UnitOfWork(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+
+        public IGenericRepository<Country> Countries => countries ??= new GenericRepository<Country>(databaseContext);
+
+        public IGenericRepository<Hotel> Hotels { get => hotels ??== new GenericRepository<Hotel>(databaseContext) };
+
+        public void Dispose()
+        {
+            databaseContext.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await databaseContext.SaveChangesAsync();
+        }
+    }
+}
